c3499b75c4a1b2071109648b34a106ce6545f933
Frey Khademi <49865527+fkhademi@users.noreply.github.com>
2022-01-29 10:31:59 +0100

    Update main.tf
    updated sg to allow ingress 10/8

--- main.tf
+++ main.tf
@@ -11,6 +11,12 @@ resource "aws_security_group" "sg" {
   ingress {
     from_port   = -1
     to_port     = -1
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
     protocol    = "ICMP"
     cidr_blocks = ["0.0.0.0/0"]
   }
