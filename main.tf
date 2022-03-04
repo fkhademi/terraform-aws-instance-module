920e17d9733280a0a0529764128f580732686f7c
Frey Khademi <49865527+fkhademi@users.noreply.github.com>
2022-03-04 14:28:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
